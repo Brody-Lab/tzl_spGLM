Brody-Lab/tzl_spGLM

commit 8ed8e542d18e827a4556cc8710279df14c61b5e9

end of time window for counting spikes
Previously, there were two possible ways to specify the end of the time window for
counting spikes on each trial:
1) with respect to a specific time after the `reference_event`
2) with respect to a specific time after the `reference_event` or
`trim_after_event`, whichever comes first

This commit adds a third option:
3) with respect to the `trim_after_event` only

src/load.jl
@@ -112,9 +112,6 @@ function loadtrials(options::Options)
 		trialindices = findall(vec(trialindices))
 	end
 	Na = floor(Int, options.time_in_trial_begin_s/options.dt)
-	Nb = ceil(Int, options.time_in_trial_end_s/options.dt)
-	default_binedges_s = (Na*options.dt):options.dt:(Nb*options.dt)
-	default_N = length(default_binedges_s)
 	Npre = ceil(Int, (options.bfs_postspike_end_s-options.bfs_postspike_begin_s)/options.dt)
 	pre_binedges = -Npre*options.dt:options.dt:0
 	spiketimes_s = vec(Cell["spiketimes_s"])
@@ -138,16 +135,17 @@ function loadtrials(options::Options)
 		else
 			reference_time_s = Trials["stateTimes"][options.reference_event][i]
 		end
-		if !isempty(options.trim_after_event)
-			lasttimestep = 1+floor(Int, (Trials["stateTimes"][options.trim_after_event][i]-reference_time_s-default_binedges_s[1])/options.dt)
-			if lasttimestep < default_N
-				binedges_s = default_binedges_s[1:lasttimestep]
-			else
-				binedges_s = default_binedges_s
-			end
-		else
-			binedges_s = default_binedges_s
+		if isnan(options.time_in_trial_end_s) && !isempty(options.trim_after_event) # the trial is truncated with respect to only `trim_after_event` and not `reference_event`
+			Nb = 1+floor(Int, (Trials["stateTimes"][options.trim_after_event][i]-reference_time_s)/options.dt)
+		elseif !isnan(options.time_in_trial_end_s) && !isempty(options.trim_after_event) # the trial is truncated with respect to both `trim_after_event` and `reference_event`
+			lasttimestep1 = 1+floor(Int, (Trials["stateTimes"][options.trim_after_event][i]-reference_time_s)/options.dt)
+			lasttimestep2 = ceil(Int, options.time_in_trial_end_s/options.dt)
+			Nb = min(lasttimestep1,lasttimestep2)
+			@assert Nb > 0
+		else # the trial is truncated with respect to only `reference_event` and not `trim_after_event`
+			Nb = ceil(Int, options.time_in_trial_end_s/options.dt)
 		end
+		binedges_s = (Na*options.dt):options.dt:(Nb*options.dt)
 		hist = StatsBase.fit(Histogram, spiketimes_s, (reference_time_s .+ binedges_s), closed=:left)
 		y = hist.weights
 		ypre = StatsBase.fit(Histogram, spiketimes_s, (reference_time_s .+ pre_binedges), closed=:left).weights
@@ -235,7 +233,7 @@ function Model(options::Options, trials::Vector{<:Trial}, 𝐰_baseline::Vector{
 		𝐛 = collect(NaN for trial in trials)
 	end
 	setnames = SPGLM.basis_function_sets()
-	basissets = collect(BasisFunctionSet(setname, options) for setname in setnames)
+	basissets = collect(BasisFunctionSet(setname, options, trials) for setname in setnames)
 	indices = UnitRange{Int}[]
 	k = 0
 	for inputname in fieldnames(SPGLM.WeightIndices)

src/basisfunctions.jl
@@ -52,18 +52,18 @@ ARGUMENT
 -`setname`: a symbol identifying the basis function set
 -`options`: a struct containing the fixed hyperparameters
 """
-function BasisFunctionSet(setname::Symbol, options::Options)
+function BasisFunctionSet(setname::Symbol, options::Options, trials::Vector{<:Trial})
 	if setname==:time_in_trial
-		begin_s = getfield(options, :time_in_trial_begin_s)
-		end_s = getfield(options, :time_in_trial_end_s)
+		begin_s = minimum(trial.timesteps_s[1] for trial in trials)
+        end_s = maximum(trial.timesteps_s[end] for trial in trials)
 	else
 		begin_s = getfield(options, Symbol("bfs_"*String(setname)*"_begin_s"))
 		end_s = getfield(options, Symbol("bfs_"*String(setname)*"_end_s"))
 	end
 	D = getfield(options, Symbol("bfs_"*String(setname)*"_D"))
-	begin_s = floor(begin_s/options.dt)*options.dt
+	begin_s = ceil(begin_s/options.dt)*options.dt
 	end_s = ceil(end_s/options.dt)*options.dt
-	timesteps_s = (begin_s+options.dt):options.dt:end_s
+	timesteps_s = begin_s:options.dt:end_s
 	N = length(timesteps_s)
 	distortion_s = getfield(options, Symbol("bfs_"*String(setname)*"_distortion_s"))
 	ηindex = ceil(Int, (distortion_s-begin_s)/options.dt)

src/types.jl
@@ -114,12 +114,12 @@ Model settings
 	reference_event::TS="cpoke_in"
 	"time, in second, after which spikes are included on each trial, aligned to the reference event on that trial."
 	time_in_trial_begin_s::TF=-2.0
-	"time, in second, before which spikes are included on each trial, aligned to the reference event on that trial."
-	time_in_trial_end_s::TF=2.0; @assert time_in_trial_end_s > time_in_trial_begin_s
+	"time, in second, before which spikes are included on each trial, aligned to the reference event on that trial. If `time_in_trial_end_s` is NaN, then the trial always end immediately before `trim_after_event`."
+	time_in_trial_end_s::TF=2.0
 	"absolute path to a MAT file containing the indices of the trials to be used"
 	trial_indices_path::TS=""
-	"event on each trial after which spikes are not counted"
-	trim_after_event::TS=""
+	"event on each trial after which spikes are not counted. If `time_in_trial_end_s` is NaN, then the trial always end immediately before `trim_after_event`, and this parameter must not be an empty string."
+	trim_after_event::TS=""; @assert !isnan(time_in_trial_end_s) || !isempty(trim_after_event)
 end
 
 """

commit 8586bbf8add6851f4eed38884b4b6b88ae77339f

fixed bug causing premature truncation
A bug was introduced in the previous commit (8ed8e54) in which the spike
train on each trial was terminated too early.

src/load.jl
@@ -135,10 +135,11 @@ function loadtrials(options::Options)
 		else
 			reference_time_s = Trials["stateTimes"][options.reference_event][i]
 		end
+		t₀ = reference_time_s + Na*options.dt
 		if isnan(options.time_in_trial_end_s) && !isempty(options.trim_after_event) # the trial is truncated with respect to only `trim_after_event` and not `reference_event`
-			Nb = 1+floor(Int, (Trials["stateTimes"][options.trim_after_event][i]-reference_time_s)/options.dt)
+			Nb = floor(Int, (Trials["stateTimes"][options.trim_after_event][i]-t₀)/options.dt)
 		elseif !isnan(options.time_in_trial_end_s) && !isempty(options.trim_after_event) # the trial is truncated with respect to both `trim_after_event` and `reference_event`
-			lasttimestep1 = 1+floor(Int, (Trials["stateTimes"][options.trim_after_event][i]-reference_time_s)/options.dt)
+			lasttimestep1 = floor(Int, (Trials["stateTimes"][options.trim_after_event][i]-t₀)/options.dt)
 			lasttimestep2 = ceil(Int, options.time_in_trial_end_s/options.dt)
 			Nb = min(lasttimestep1,lasttimestep2)
 			@assert Nb > 0
@@ -162,7 +163,6 @@ function loadtrials(options::Options)
 			pose_sorted = collect(zeros(0) for i=1:length(trialindices))
 		end
 		@assert !isnan(Trials["stateTimes"]["cpoke_out"][i])
-		t₀ = reference_time_s + binedges_s[1]
 		fixation_timestep = ceil(Int, (Trials["stateTimes"]["cpoke_in"][i] - t₀)/options.dt)
 		movement_timestep = ceil(Int, (Trials["stateTimes"]["cpoke_out"][i] - t₀)/options.dt)
 		leftclicks_s = Trials["leftBups"][i] .+ Trials["stateTimes"]["clicks_on"][i] .- t₀